8d7be65b1c2b33a21a1d1cc0e7b033373867867c
Sylvain Rabot <sylvain.rabot@f-secure.com>
2014-10-09 15:08:16 +0200

    perl obfuscation
    Signed-off-by: Sylvain Rabot <sylvain.rabot@f-secure.com>

--- bash.sh
+++ bash.sh
@@ -2,3 +2,6 @@
 
 # obfuscate with perl
 echo "this is a test" | perl -MDigest::MD5 -ne 'print "$1 ".Digest::MD5::md5_hex($2)." $3\n" if /^([^ ]+) ([^ ]+) (.+)$/'
+
+# obfuscate strings which look like uniq id
+perl -MDigest::MD5 -pe 's/([a-zA-Z0-9-\*]{20,})/Digest::MD5::md5_hex($1);/eg' < input > output
